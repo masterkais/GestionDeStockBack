930c05493ca0f15ce9a8f85a0ed43f9179545c1b
ktrabelsi <ktrabelsi@spark.local>
2023-01-13 10:26:21 +0100

    add-exception

--- src/main/java/com/kais/gestiondestock/exception/EntityNotFoundException.java
+++ src/main/java/com/kais/gestiondestock/exception/EntityNotFoundException.java
@@ -1,4 +1,22 @@
 package com.kais.gestiondestock.exception;
 
-public class EntityNotFoundException {
+import lombok.Data;
+
+@Data
+public class EntityNotFoundException extends RuntimeException {
+    private ErrorCodes errorCodes;
+    public EntityNotFoundException(String message){
+       super(message);
+    }
+    public EntityNotFoundException(String message,Throwable cause){
+        super(message,cause);
+    }
+    public EntityNotFoundException(String message,Throwable cause,ErrorCodes errorCodes){
+        super(message,cause);
+        this.errorCodes=errorCodes;
+    }
+    public EntityNotFoundException(String message,ErrorCodes errorCodes){
+        super(message);
+        this.errorCodes=errorCodes;
+    }
 }
---
+++ src/main/java/com/kais/gestiondestock/exception/InvalidEntityException.java
@@ -0,0 +1,25 @@
+package com.kais.gestiondestock.exception;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class InvalidEntityException extends RuntimeException{
+    private ErrorCodes errorCodes;
+    private List<String> errors;
+    public InvalidEntityException(String message){
+        super(message);
+    }
+    public InvalidEntityException(String message,Throwable cause){
+        super(message,cause);
+    }
+    public InvalidEntityException(String message,Throwable cause,ErrorCodes errorCodes){
+        super(message,cause);
+        this.errorCodes=errorCodes;
+    }
+    public InvalidEntityException(String message,Throwable cause,ErrorCodes errorCodes,List<String> errors){
+        super(message,cause);
+        this.errorCodes=errorCodes;
+        this.errors=errors;
+    }
+}
